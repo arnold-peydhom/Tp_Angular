dd7954851a73746a49f06e2940f68e954c97ccfa
arnold peydhom <arnoldpeydhom276@gmail.com>
2025-01-15 23:13:51 +0100

    exercice7 terminer

--- Tp/src/app/app.module.ts
+++ Tp/src/app/app.module.ts
@@ -6,18 +6,21 @@ import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule} from "@angular/forms";
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     RouterModule.forRoot([
-      { path:'courses', component:CoursesComponent}
+      { path:'courses', component:CoursesComponent},
+      { path:"about", component:AboutComponent}
     ])
   ],
   providers: [
